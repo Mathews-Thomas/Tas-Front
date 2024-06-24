99e002de9f4d788d4b6edc767ebef21486c8440d
ABHIJITH TA <90981851+AbhijithTA@users.noreply.github.com>
2024-06-24 13:02:00 +0530

    dept error solved in medicine edit

--- src/components/ReviewPanel/medicine/MedicineEditForm.jsx
+++ src/components/ReviewPanel/medicine/MedicineEditForm.jsx
@@ -137,7 +137,7 @@ const MedicineEditForm = ({
         setSubmitting(false);
         resetForm();
         onClose();
-        refresh();
+       
       } catch (error) {
         console.error("Error updating form data:", error);
       }
